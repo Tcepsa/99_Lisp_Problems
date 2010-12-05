0c8452ae19b9fdcb13421dc7f2cf701b1c1a40a3
Joel Jorgensen <tcepsa@gmail.com>
2010-12-04 20:57:12 -0500

    Added Swank dependency for REPL in Emacs.

--- clojure/project.clj
+++ clojure/project.clj
@@ -1,4 +1,5 @@
 (defproject clojure "1.0.0-SNAPSHOT"
-  :description "FIXME: write"
+  :description "A project for collaborative work on solving the L-99 Lisp problems in Clojure"
   :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]])
+                 [org.clojure/clojure-contrib "1.2.0"]]
+  :dev-dependencies [[swank-clojure "1.2.1"]])
