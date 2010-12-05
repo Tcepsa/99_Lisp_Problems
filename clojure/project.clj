a4aaa340ac16e445d14f146c6f42f87e19238ab4
Joel Jorgensen <tcepsa@gmail.com>
2010-12-04 21:26:42 -0500

    Shuffled stuff around so as not to conflict with the main clojure namespace >_<

--- clojure/project.clj
+++ clojure/project.clj
@@ -1,4 +1,4 @@
-(defproject clojure "1.0.0-SNAPSHOT"
+(defproject C-99 "1.0.0-SNAPSHOT"
   :description "A project for collaborative work on solving the L-99 Lisp problems in Clojure"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]]
